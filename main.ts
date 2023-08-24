1f5c4db0edad959d97bf7226a4eb4da2f55e0674
hanno1seibod <74058712+hanno1seibod@users.noreply.github.com>
2023-08-24 17:42:48 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 function macheEtwas () {
-    BinärzahlZahl = 0
     for (let Index = 0; Index <= Binärzahl.length - 1; Index++) {
         if (parseFloat(Binärzahl.substr(Index, 1)) == 0) {
             if (Binärzahl.substr(Index, 1).includes(" ")) {
@@ -45,17 +44,24 @@ function macheEtwas () {
         }
     }
     basic.setLedColor(0x000000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
     control.reset()
 }
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     aGedrueckt = 1
     basic.pause(2000)
     basic.showNumber(3)
-    basic.pause(1500)
+    basic.pause(750)
     basic.showNumber(2)
-    basic.pause(1500)
+    basic.pause(750)
     basic.showNumber(1)
-    basic.pause(1500)
+    basic.pause(750)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -63,21 +69,20 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
         . . . . .
         . . . . .
         `)
+    basic.pause(750)
     macheEtwas()
 })
 function sendeNachricht () {
     aGedrueckt = 0
-    for (let index = 0; index < 2; index++) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showIcon(IconNames.SmallSquare)
-        basic.showIcon(IconNames.Square)
-    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
     while (aGedrueckt == 0) {
         basic.showIcon(IconNames.ArrowWest)
         basic.pause(1000)
@@ -92,9 +97,8 @@ function sendeNachricht () {
     }
 }
 let aGedrueckt = 0
-let BinärzahlZahl = 0
 let Binärzahl = ""
-Binärzahl = "00101"
+Binärzahl = "0010100"
 sendeNachricht()
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
