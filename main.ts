c8b426c8dd2141e325ffe80ca3c55951ed2f8416
hanno1seibod <74058712+hanno1seibod@users.noreply.github.com>
2023-08-24 16:20:01 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 function macheEtwas () {
     BinärzahlZahl = 0
     for (let Index = 0; Index <= Binärzahl.length - 1; Index++) {
-        basic.pause(1000)
-        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        basic.pause(1500)
+        music.playTone(523, music.beat(BeatFraction.Sixteenth))
         if (parseFloat(Binärzahl.substr(Index, 1)) == 0) {
             basic.setLedColor(0x000000)
         } else {
@@ -13,9 +13,16 @@ function macheEtwas () {
     basic.setLedColor(0x000000)
 }
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    macheEtwas()
     aGedrueckt = 1
     basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    macheEtwas()
 })
 function sendeNachricht () {
     aGedrueckt = 0
@@ -46,5 +53,10 @@ function sendeNachricht () {
 let aGedrueckt = 0
 let BinärzahlZahl = 0
 let Binärzahl = ""
-Binärzahl = "10011"
+Binärzahl = "1101011000000011110101110011"
 sendeNachricht()
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        control.reset()
+    }
+})
